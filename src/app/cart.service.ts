d76b9361e89a1e004c90b7474c6d5980e6e62bee
FakeFabianWorkstation <romanreigns397@gmail.com>
2023-12-28 01:25:32 +0530

    First commit

---
+++ src/app/cart.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Product } from './products';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  cartItems: Product[] = [];
+  
+  constructor(private http: HttpClient) { }
+
+  addCartItem(product: Product) {
+    this.cartItems.push(product);
+  }
+
+  getItems() {
+    return this.cartItems;
+  }
+  
+  clearCart() {
+    this.cartItems = [];
+    return this.cartItems;
+  }
+
+  getShippingPrices() {
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
+  }
+}
